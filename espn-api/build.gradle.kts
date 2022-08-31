056d576d6baec48377b06a4f323b39c54539a0ef
Omar <omar25shaarawi@gmail.com>
2022-08-30 23:32:28 -0500

    first commit

---
+++ espn-api/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    `java-library`
+
+    id("idea")
+    id("maven-publish")
+
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.kotlinSerialization)
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.detekt)
+}
+
+group = "com.fantasy.football"
+version = System.getenv("VERSION")
+
+repositories {
+    mavenCentral()
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    implementation(libs.retrofit)
+    implementation(libs.gson)
+    implementation(libs.retrofit.kotlinx.serialization)
+    implementation(libs.kotlinx.serialization)
+    implementation(libs.kotlin.logging)
+    implementation(libs.slf4j)
+    implementation(libs.detektFormatting)
+
+    testImplementation(libs.bundles.kotest)
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/omarshaarawi/espn-ff-kotlin")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
+}
---
+++ yahoo-api/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    `java-library`
+
+    id("idea")
+    id("maven-publish")
+
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.kotlinSerialization)
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.detekt)
+}
+
+group = "com.fantasy.football"
+version = System.getenv("VERSION")
+
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.lombok)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.jackson)
+    implementation(libs.retrofit.jaxb)
+    implementation(libs.retrofit.kotlinx.serialization)
+    implementation(libs.kotlin.logging)
+    implementation(libs.kotlinx.serialization)
+    implementation(libs.slf4j)
+    implementation(libs.kotlin.logging)
+    implementation(libs.detektFormatting)
+
+    testImplementation(libs.bundles.kotest)
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/omarshaarawi/espn-ff-kotlin")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
+}
